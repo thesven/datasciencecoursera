b8f75447e658c4b9542148421eefdbe318a35fdb
Michael Svendsen <michaelsvendsen@Michaels-MacBook-Pro-3.local>
2015-03-19 09:32:52 -0400

    rename of folder

--- R-Programming/Week 2 Assignement/submission.R
+++
@@ -1,4 +0,0 @@
-#set the working directory
-setwd("/Volumes/StorageLocker/Coursera/Data Science/datasciencecoursera/R-Programming/Week 2 Assignement")
-source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
-submit()
